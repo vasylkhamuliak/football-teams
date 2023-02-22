07b9c628e36039c97a0644a498f9f11d36918a5e
Vasyl Khamuliak Raketech <vasyl.khamuliak@rt-externals.com>
2023-02-22 14:28:09 +0200

    [VUE] Add team generator on vue.js

--- index.php
+++ index.php
@@ -1,96 +1,61 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Футбольні команди</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <link rel="stylesheet" href="style.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Team generator</title>
 </head>
 <body>
-	<form action="generate-teams.php" method="post">
-		<label for="numPlayers">Кількість гравців:</label>
-		<input type="number" id="numPlayers" name="numPlayers" min="1" max="100" required><br>
+  <h1><?php echo 'Team generator:' ?></h1>
+  <div id="app">
+  <form>
+		<div v-for="(player, index) in players" :key="index" class="player">
+		  <input v-model.trim="player.name" type="text" placeholder="Name">
+		  <input v-model.number="player.level" type="number" min="1" max="10" placeholder="Level">
+		  <button @click="removePlayer(index)">Remove</button>
+		</div>
+    <button type="button" @click="addPlayer">Add player</button>
+    <div class="team-number">
+      <label>Number of teams:</label>
+      <input v-model.number="teamNumber" type="number" min="2">
+    </div>
+    <button type="button" @click="generateTeams">Generate teams</button>
+  </form>
 
-		<?php
-			for ($i=1; $i <= 4; $i++) {
-				echo '<label for="name'.$i.'">Гравець '.$i.':</label>
-				<input type="text" id="name'.$i.'" name="name'.$i.'" required>
-				<label for="rating'.$i.'">Рейтинг (1-10):</label>
-				<input type="number" id="rating'.$i.'" name="rating'.$i.'" min="1" max="10" required><br>';
-			}
-		?>
+	<div v-if="teams.length">
+    <h2>Generated teams:</h2>
+    <table v-for="(team, index) in teams" :key="index">
+      <caption>Team {{ index + 1 }}</caption>
+      <thead>
+        <tr>
+          <th>Player</th>
+          <th>Level</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(player, playerIndex) in team" :key="playerIndex">
+          <td>{{ player.name }}</td>
+          <td>{{ player.level }}</td>
+        </tr>
+      </tbody>
+			<tfoot>
+        <tr>
+          <td>Total:</td>
+          <td>{{ team.reduce((sum, player) => sum + parseInt(player.level), 0) }}</td>
+        </tr>
+        <tr>
+				<td>Average:</td>
+				<td>{{ ((team.reduce((sum, player) => sum + parseInt(player.level), 0)) / team.length).toFixed(1) }}</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 
-		<label for="numTeams">Кількість команд:</label>
-		<select id="numTeams" name="numTeams">
-			<option value="2">2</option>
-			<option value="4">4</option>
-			<option value="6">6</option>
-			<option value="8">8</option>
-		</select><br>
+</div>
 
-		<input type="submit" value="Генерувати команди">
-	</form>
-
-	<script>
-		// отримуємо доступ до форми та кнопки генерації команд
-const form = document.querySelector('#players-form');
-const generateButton = document.querySelector('#generate-button');
-
-// додаємо обробник подій на кнопку генерації команд
-generateButton.addEventListener('click', generateTeams);
-
-function generateTeams() {
-  // отримуємо кількість команд, що вибрав користувач
-  const teamCount = parseInt(document.querySelector('#team-count').value);
-  
-  // отримуємо список гравців та їх рейтинг
-  const playerList = [];
-  const ratingList = [];
-  const players = document.querySelectorAll('.player-name');
-  const ratings = document.querySelectorAll('.player-rating');
-  
-  // заповнюємо масиви гравців та їх рейтинг
-  for (let i = 0; i < players.length; i++) {
-    playerList.push(players[i].value);
-    ratingList.push(parseInt(ratings[i].value));
-  }
-  
-  // розподіляємо гравців на команди
-  const teams = [];
-  for (let i = 0; i < teamCount; i++) {
-    teams.push([]);
-  }
-  
-  // сортуємо гравців за рейтингом
-  const sortedPlayers = playerList.slice().sort((a, b) => {
-    const aIndex = playerList.indexOf(a);
-    const bIndex = playerList.indexOf(b);
-    return ratingList[bIndex] - ratingList[aIndex];
-  });
-  
-  // розподіляємо гравців на команди
-  let teamIndex = 0;
-  for (let i = 0; i < sortedPlayers.length; i++) {
-    teams[teamIndex].push(sortedPlayers[i]);
-    teamIndex = (teamIndex + 1) % teamCount;
-  }
-  
-  // виводимо команди на екран
-  const teamContainer = document.querySelector('#team-container');
-  teamContainer.innerHTML = '';
-  for (let i = 0; i < teamCount; i++) {
-    const teamHeading = document.createElement('h2');
-    teamHeading.textContent = `Команда ${i + 1}`;
-    const teamList = document.createElement('ul');
-    for (let j = 0; j < teams[i].length; j++) {
-      const playerItem = document.createElement('li');
-      playerItem.textContent = teams[i][j];
-      teamList.appendChild(playerItem);
-    }
-    const teamDiv = document.createElement('div');
-    teamDiv.appendChild(teamHeading);
-    teamDiv.appendChild(teamList);
-    teamContainer.appendChild(teamDiv);
-  }
-}
-
-	</script>
 </body>
+<script src="scripts.js"></script>
 </html>
